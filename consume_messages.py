e1fdc8d8d1fbe6263d3ccf64d959ea8d45c7b948
DenicaKj <dkorvezir@gmail.com>
2024-02-06 02:09:22 +0100

    fixes

--- consume_messages.py
+++ consume_messages.py
@@ -1,26 +1,19 @@
-from confluent_kafka import Consumer
+from kafka import KafkaConsumer
 
-c = Consumer({
-    'bootstrap.servers': 'localhost:29092',
-    'group.id': 'my_consumer_group',
-    'auto.offset.reset': 'earliest'
-})
+bootstrap_servers = 'localhost:29092'
+topic = 'health_data_predicted'
 
-c.subscribe(['health_data_predicted'])
+consumer = KafkaConsumer(topic,
+                         bootstrap_servers=bootstrap_servers,
+                         group_id='my_consumer_group',
+                         auto_offset_reset='earliest',
+                         enable_auto_commit=False)
 
 try:
-	while True:
-		msg = c.poll(1.0)
-
-		if msg is None:
-			continue
-		if msg.error():
-			print("Consumer error: {}".format(msg.error()))
-			continue
-
-		print('Received message: {}'.format(msg.value().decode('utf-8')))
-
+	for message in consumer:
+		print(f"Received message: {message.value.decode('utf-8')}")
 except KeyboardInterrupt:
 	pass
 finally:
-	c.close()
+	# Close the consumer
+	consumer.close()
--- produce_messages.py
+++ produce_messages.py
@@ -1,24 +1,20 @@
 import json
 import time
-from confluent_kafka import Producer
+from kafka import KafkaProducer
 
 import random
 import pandas as pd
 
-producer_config = {
-    'bootstrap.servers': 'localhost:29092',
-    'security.protocol': 'PLAINTEXT',
-}
 
-producer = Producer(producer_config)
+producer = KafkaProducer(bootstrap_servers='localhost:29092', security_protocol="PLAINTEXT")
 
-df = pd.read_csv("data/online.csv")
+df = pd.read_csv("online.csv")
 
 for i, row in df.iterrows():
     record = row.to_json()
-    producer.produce(
+    print(json.dumps(record))
+    producer.send(
         topic="health_data",
         value=record.encode("utf-8")
     )
-    producer.flush()
     time.sleep(random.randint(500, 2000) / 1000.0)
